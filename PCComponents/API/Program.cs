701a55e6b1dfabfa20cfa9c29359d716cf42eb2a
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-10-22 19:43:59 +0300

    project base was added

--- PCComponents/API/Program.cs
+++ PCComponents/API/Program.cs
@@ -1,6 +1,30 @@
+using Api.Modules;
+using Application;
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.SetupServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.InitialiseDb();
+app.MapControllers();
 
 app.Run();
+
+public partial class Program;
