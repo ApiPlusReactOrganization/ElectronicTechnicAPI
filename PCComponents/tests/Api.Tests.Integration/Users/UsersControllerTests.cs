92f5793ce47b0e647bc352e6439dc124d1188141
mishalolka <mykhailo.lukianchuk@oa.edu.ua>
2024-11-19 00:05:12 +0200

    added tests for products ,user

--- PCComponents/tests/Api.Tests.Integration/Users/UsersControllerTests.cs
+++ PCComponents/tests/Api.Tests.Integration/Users/UsersControllerTests.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Net.Http.Json;
+using Api.Dtos.Authentications;
 using Api.Dtos.Users;
 using FluentAssertions;
 using Tests.Common;
@@ -9,20 +11,97 @@ namespace Api.Tests.Integration.Users;
 
 public class UsersControllerTests(IntegrationTestWebFactory factory) : BaseIntegrationTest(factory)
 {
-    // [Fact]
-    // public async Task ShouldDeleteUser()
-    // {
-    //     // Arrange
-    //     var userId = UsersData.TestUser.id;
-    //
-    //     // Act
-    //     var response = await Client.DeleteAsync($"users/{userId}");
-    //
-    //     // Assert
-    //     response.IsSuccessStatusCode.Should().BeTrue();
-    // }
+    [Fact]
+    public async Task ShouldGetAllUsers()
+    {
+        // Arrange
+        var signUpRequest = UsersData.SignUpMainUser;
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", signUpRequest);
+        signUpResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має успішно зареєструватись");
+
+        // Act
+        var usersResponse = await Client.GetFromJsonAsync<UserDto[]>("users");
+
+        // Assert
+        usersResponse.Should().NotBeNullOrEmpty();
+        usersResponse!.Any(u => u.email == signUpRequest.email).Should().BeTrue("Користувач має бути у списку");
+    }
 
     [Fact]
+    public async Task ShouldDeleteUser()
+    {
+        // Arrange
+        var signUpRequest = UsersData.SignUpDeleteUser;
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", signUpRequest);
+        signUpResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має успішно зареєструватись");
+
+
+        var users = await Client.GetFromJsonAsync<UserDto[]>("users");
+        var mainUser = users!.FirstOrDefault(u => u.email == signUpRequest.email);
+        mainUser.Should().NotBeNull("Користувач має існувати");
+
+        // Act
+        var deleteResponse = await Client.DeleteAsync($"users/{mainUser!.id}");
+
+        // Assert
+        deleteResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має бути успішно видалений");
+    }
+
+    [Fact]
+    public async Task ShouldUpdateUserRoles()
+    {
+        // Arrange
+        var signUpRequest = UsersData.SignUpUpdateUserRoles;
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", signUpRequest);
+        signUpResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має успішно зареєструватись");
+
+
+        var users = await Client.GetFromJsonAsync<UserDto[]>("users");
+        var mainUser = users!.FirstOrDefault(u => u.email == signUpRequest.email);
+        mainUser.Should().NotBeNull("Користувач має існувати");
+
+ 
+        var newRoles = new List<RoleDto>
+        {
+            new("Administrator")
+        };
+
+        // Act
+        var updateRolesResponse = await Client.PutAsJsonAsync($"users/UpdateRoles/{mainUser!.id}", newRoles);
+
+        // Assert
+        updateRolesResponse.IsSuccessStatusCode.Should().BeTrue("Ролі мають бути успішно оновлені");
+    }
+
+    [Fact]
+    public async Task ShouldUploadUserImage()
+    {
+        // Arrange
+        var signUpRequest = UsersData.SignUpUploadUser;
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", signUpRequest);
+        signUpResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має успішно зареєструватись");
+
+
+        var users = await Client.GetFromJsonAsync<UserDto[]>("users");
+        var mainUser = users!.FirstOrDefault(u => u.email == signUpRequest.email);
+        mainUser.Should().NotBeNull("Користувач має існувати");
+        
+        using var imageContent = new ByteArrayContent(new byte[0]);
+        imageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+        var content = new MultipartFormDataContent
+        {
+            { imageContent, "imageFile", "test.png" }
+        };
+
+        // Act
+        var uploadImageResponse = await Client.PutAsync($"users/Image/{mainUser!.id}", content);
+
+        // Assert
+        uploadImageResponse.IsSuccessStatusCode.Should().BeTrue("Зображення має бути успішно завантажене");
+    }
+
+
+        [Fact]
     public async Task ShouldNotDeleteNonExistingUser()
     {
         // Arrange
@@ -33,6 +112,82 @@ public class UsersControllerTests(IntegrationTestWebFactory factory) : BaseInteg
 
         // Assert
         response.IsSuccessStatusCode.Should().BeFalse();
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound, "Користувача не існує");
+    }
+
+    [Fact]
+    public async Task ShouldNotUpdateRolesForNonExistingUser()
+    {
+        // Arrange
+        var nonExistingUserId = Guid.NewGuid();
+        var newRoles = new List<RoleDto>
+        {
+            new("Administrator")
+        };
+
+        // Act
+        var response = await Client.PutAsJsonAsync($"users/UpdateRoles/{nonExistingUserId}", newRoles);
+
+        // Assert
+        response.IsSuccessStatusCode.Should().BeFalse();
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound, "Користувач не існує");
+    }
+
+    [Fact]
+    public async Task ShouldNotUpdateRolesWithInvalidRoles()
+    {
+        // Arrange
+        var signUpRequest = UsersData.SignUpNotUpdateUserRoles;
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", signUpRequest);
+        signUpResponse.IsSuccessStatusCode.Should().BeTrue("Користувач має успішно зареєструватись");
+
+        // Отримуємо ID користувача
+        var users = await Client.GetFromJsonAsync<UserDto[]>("users");
+        var mainUser = users!.FirstOrDefault(u => u.email == signUpRequest.email);
+        mainUser.Should().NotBeNull("Користувач має існувати");
+
+        // Порожній список ролей
+        var emptyRoles = new List<RoleDto>();
+
+        // Act
+        var updateRolesResponse = await Client.PutAsJsonAsync($"users/UpdateRoles/{mainUser!.id}", emptyRoles);
+
+        // Assert
+        updateRolesResponse.IsSuccessStatusCode.Should().BeFalse();
+        updateRolesResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest, "Ролі не можуть бути порожніми");
+    }
+
+    [Fact]
+    public async Task ShouldNotUploadImageForNonExistingUser()
+    {
+        // Arrange
+        var nonExistingUserId = Guid.NewGuid();
+
+        using var imageContent = new ByteArrayContent(new byte[0]);
+        imageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+        var content = new MultipartFormDataContent
+        {
+            { imageContent, "imageFile", "test.png" }
+        };
+
+        // Act
+        var response = await Client.PutAsync($"users/Image/{nonExistingUserId}", content);
+
+        // Assert
+        response.IsSuccessStatusCode.Should().BeFalse();
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound, "Користувач не існує");
+    }
+    
+    [Fact]
+    public async Task ShouldNotRegisterUserWithInvalidData()
+    {
+        // Arrange
+        var invalidSignUpRequest = UsersData.invalidSignUpRequest;
+        // Act
+        var signUpResponse = await Client.PostAsJsonAsync("account/signup", invalidSignUpRequest);
+
+        // Assert
+        signUpResponse.IsSuccessStatusCode.Should().BeFalse();
+        signUpResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest, "Запит має бути некоректним");
     }
 }
--- PCComponents/tests/Tests.Data/UsersData.cs
+++ PCComponents/tests/Tests.Data/UsersData.cs
@@ -1,3 +1,4 @@
+using Api.Dtos.Authentications;
 using Api.Dtos.Users;
 using Domain.Authentications.Users;
 
@@ -5,11 +6,40 @@ namespace Tests.Data;
 
 public static class UsersData
 {
+    public static UserDto MainUser => new(
+        id: Guid.NewGuid(),
+        email: "mainuser@example.com",
+        name: "Main User",
+        image : null,
+        roles: new List<RoleDto> { new("User") });
+
+    public static SignUpDto SignUpMainUser => new(
+        email: "mainuser@gmail.com",
+        password: "MainUserPassword123!",
+        name: "Main User");
     
-    public static UserDto TestUser => new(
-        Guid.NewGuid(),
-         "testuser@example.com",
-       "Test User",
-        null,
-        new List<RoleDto> { new ("User") });
+    public static SignUpDto SignUpDeleteUser => new(
+        email: "deleteuser@gmail.com",
+        password: "DeleteUserPassword123!",
+        name: "Delete User");
+    
+    public static SignUpDto SignUpUpdateUserRoles => new(
+        email: "updateuserRoles@gmail.com",
+        password: "UpdateUserRolesPassword123!",
+        name: "UpdateRoles User");
+    public static SignUpDto SignUpNotUpdateUserRoles => new(
+        email: "notupdateuserRoles@gmail.com",
+        password: "notUpdateUserRolesPassword123!",
+        name: "notUpdateRoles User");
+    
+    public static SignUpDto SignUpUploadUser => new(
+        email: "uploadUseruser@gmail.com",
+        password: "UploadUserUserPassword123!",
+        name: "UploadUser User");
+    
+    public static SignUpDto invalidSignUpRequest => new(
+        email: "",
+        password: "123",
+        name: "Invalid User"
+        );
 }
