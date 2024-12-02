43e44b0d763fed03cc9f34337b0a25e59eadb9d6
mishalolka299 <mykhailo.lukianchuk@oa.edu.ua>
2024-12-02 23:53:12 +0200

    added filter queries

--- PCComponents/src/Api/Controllers/ProductsController.cs
+++ PCComponents/src/Api/Controllers/ProductsController.cs
@@ -65,6 +65,42 @@ public class ProductsController(ISender sender, IProductQueries productQueries) 
 
         return products.Select(ProductDto.FromDomainModel).ToList();
     }
+    [HttpGet("under-manufacturers")]
+    public async Task<ActionResult<IReadOnlyList<ProductDto>>> GetByManufacturers(
+        [FromQuery] List<Guid> manufacturerIds,
+        CancellationToken cancellationToken)
+    {
+        var ids = manufacturerIds.Select(id => new ManufacturerId(id)).ToList();
+        var products = await productQueries.GetProductsByManufacturers(ids, cancellationToken);
+
+        return products.Select(ProductDto.FromDomainModel).ToList();
+    }
+    [HttpGet("filter")]
+    public async Task<ActionResult<IReadOnlyList<ProductDto>>> FilterProducts(
+        [FromQuery] Guid? categoryId,
+        [FromQuery] List<Guid>? manufacturerIds,
+        [FromQuery] string? name,
+        [FromQuery] decimal? minPrice,
+        [FromQuery] decimal? maxPrice,
+        [FromQuery] int? minStockQuantity,
+        [FromQuery] int? maxStockQuantity,
+        CancellationToken cancellationToken)
+    {
+        var products = await productQueries.FilterProducts(
+            categoryId,
+            manufacturerIds,
+            name,
+            minPrice,
+            maxPrice,
+            minStockQuantity,
+            maxStockQuantity,
+            cancellationToken);
+
+        return products.Select(ProductDto.FromDomainModel).ToList();
+    }
+
+
+
 
     [HttpGet("get-by-id/{productId:guid}")]
     public async Task<ActionResult<ProductDto>> Get([FromRoute] Guid productId, CancellationToken cancellationToken)
